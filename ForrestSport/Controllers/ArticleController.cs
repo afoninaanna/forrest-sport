7c20d9d3355195ff03c64256f0c3562b2b51b218
afoninaanna <afoninaa903@gmail.com>
2023-06-12 20:06:16 +0400

    add: article controller and cors in program

---
+++ ForrestSport/Controllers/ArticleController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ForrestSport.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ForrestSport.Controllers
+{
+    [Route("api/article")]
+    [ApiController]
+    public class ArticleController : ControllerBase
+    {
+        private readonly ArticleDbContext _dbContext;
+
+        public ArticleController(ArticleDbContext articleDbContext)
+        {
+            _dbContext = articleDbContext;
+        }
+        
+        [HttpGet]
+        public ActionResult<IEnumerable<Article>> GetArticles()
+        {
+            return _dbContext.Articles;
+        }
+
+        [HttpGet("{articleId:int}")]
+        public async Task<ActionResult<Article>> GetById(int articleId)
+        {
+            var article = await _dbContext.Articles.FindAsync(articleId);
+            return article;
+        }
+
+        
+        [HttpPost]
+        public async Task<ActionResult> Create(Article article)
+        {
+            await _dbContext.Articles.AddAsync(article);
+            await _dbContext.SaveChangesAsync();
+            return Ok();
+        }
+        
+        [HttpPut]
+        public async Task<ActionResult> Update(Article article)
+        {
+            _dbContext.Articles.Update(article);
+            await _dbContext.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{articleId:int}")]
+        public async Task<ActionResult> Delete(int articleId)
+        {
+            var article = await _dbContext.Articles.FindAsync(articleId);
+            _dbContext.Articles.Remove(article);
+            await _dbContext.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
+
--- ForrestSport/Program.cs
+++ ForrestSport/Program.cs
@@ -3,9 +3,33 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.WithOrigins("https://localhost:44466").AllowAnyHeader(); 
+        });
+});
+
+
+
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
+
+/* Database Context Dependency Injection */
+
+
+var dbHost = "localhost";
+var dbName = "db_article";
+var dbPassword = "12345678";
+
+
+var connectionString = $"server={dbHost};port=3306;database={dbName};user=root;password={dbPassword}";
+builder.Services.AddDbContext<ArticleDbContext>(o => o.UseMySQL(connectionString));
+
+/* ====================================== */
 
 var app = builder.Build();
 
@@ -19,6 +43,8 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+
+app.UseCors();
 
 app.MapControllerRoute(
     name: "default",

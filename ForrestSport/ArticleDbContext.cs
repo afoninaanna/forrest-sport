ddef82c99f6d8386ae25a0089019d74445d2704a
afoninaanna <afoninaa903@gmail.com>
2023-06-12 19:48:13 +0400

    add: article model and DbContext

---
+++ ForrestSport/ArticleDbContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using ForrestSport.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace ForrestSport
+{
+	public class ArticleDbContext : DbContext
+	{
+		public DbSet<Article> Articles { get; set; }
+		public ArticleDbContext(DbContextOptions<ArticleDbContext> dbContextOptions) : base(dbContextOptions)
+		{
+			try
+			{
+				var databaseCreator = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
+				if (databaseCreator != null)
+				{
+					// Create Database if cannot connect
+					if (!databaseCreator.CanConnect()) databaseCreator.Create();
+
+					// Create Tables if no tables exist
+					if (!databaseCreator.HasTables()) databaseCreator.CreateTables();
+				}
+			}
+			catch(Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+		}
+		
+	}
+}
+
---
+++ ForrestSport/Models/Article.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ForrestSport.Models
+{
+	[Table("article")]
+	public class Article
+	{
+		[Key]
+		[Column("article_id")]
+		public int Id { get; set; }
+
+		[Column("article_image")]
+		public string Image { get; set; }
+
+		[Column("article_title")]
+		public string Title { get; set; }
+
+		[Column("article_annotation")]
+		public string Annotation { get; set; }
+
+		[Column("article_tags")]
+        public string Tags { get; set; }
+
+		[Column("article_category")]
+		public string Category { get; set; }
+
+        [Column("article_content")]
+        public string Content { get; set; }
+
+        [Column("article_author")]
+        public string Author { get; set; }
+    }
+}
+
